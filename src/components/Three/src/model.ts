09d580f4827f8e854ec356dfbd4cc20fb315bced
cxwii <1471749154@qq.com>
2023-10-09 22:53:08 +0800

    three纹理加载的一个小坑(首次加载纹理不渲染)

--- src/components/Three/src/model.ts
+++ src/components/Three/src/model.ts
@@ -10,6 +10,10 @@ import earth from '../assets/earth.jpeg'
 // 纹理加载
 const texLoader = new TextureLoader()
 const texture = texLoader.load(earth)
+// 这里应该要这样来加载完后再渲染一次,不然首次加载无法加载纹理,不过这里我分离模型了就麻烦修改了
+// const texture = texLoader.load(earth, () => {
+//   renderer.render(scene, camera)
+// })
 
 const geometry = new SphereGeometry(100)
 // const geometry = new BoxGeometry(100, 100, 100)
